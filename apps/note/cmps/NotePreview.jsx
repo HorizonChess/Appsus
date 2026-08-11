7e640c352fb972eccb7d1813c16aac22e12c791e
asanir96 <asanir96@gmail.com>
2026-08-11 06:16:07 +0300

    Add NoteTodos component to DynamicNote for rendering todo notes

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -15,10 +15,25 @@ function NoteTxt({ info }) {
     return <p>{txt}</p>
 }
 
+function NoteTodos({ info }) {
+    const { title, todos } = info
+
+    return <div>
+        <h3>{title}</h3>
+        {todos.map(todo => {
+            return <div key={todo.txt}>
+                <input type="checkbox" id={todo.txt} name={todo.txt} />
+                <label htmlFor={todo.txt}>{todo.txt}</label>
+
+            </div>
+        })}
+    </div>
+}
 
 function DynamicNote(props) {
     const cmpMap = {
-        'NoteTxt': <NoteTxt {...props} />
+        'NoteTxt': <NoteTxt {...props} />,
+        'NoteTodos': <NoteTodos {...props} />
     }
 
     return cmpMap[props.type]
