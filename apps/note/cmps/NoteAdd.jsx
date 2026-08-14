f6417693c323d881532b06138482f0ecaf6b5c4a
asanir96 <asanir96@gmail.com>
2026-08-14 05:58:49 +0300

    Implement NoteTodos functionality in NoteAdd component and enhance styling

--- apps/note/cmps/NoteAdd.jsx
+++ apps/note/cmps/NoteAdd.jsx
@@ -4,13 +4,17 @@ export function NoteAdd({ emptyNote, addNote }) {
     const [inputType, setInputType] = useState(null)
     const [emptyNoteToEdit, setEmptyNoteToEdit] = useState(emptyNote)
 
-    console.log('emptyNoteToEdit.info', emptyNoteToEdit.info)
     useEffect(() => {
         setEmptyNoteToEdit({ ...emptyNoteToEdit, type: inputType })
     }, [inputType])
 
-    function onInputChange(emptyNoteToEdit, newInfo) {
+    function onInputChange(demptyNoteToEdit, newInfo, todo) {
+
         setEmptyNoteToEdit({ ...emptyNoteToEdit, info: newInfo })
+    }
+
+    function onTodoChange(value, todo) {
+        const newTodo = { ...todo, txt: +value }
     }
 
 
@@ -21,6 +25,13 @@ export function NoteAdd({ emptyNote, addNote }) {
             onClick={() => setInputType('NoteTxt')}
             type="text"
         />
+
+        <button
+            className="change-type-btn"
+            onClick={() => setInputType('NoteTodos')}
+        >
+            <i class="fa-regular fa-square-check"></i>
+        </button>
     </fieldset >
 
     return <fieldset className="add-note">
@@ -41,10 +52,63 @@ export function NoteAdd({ emptyNote, addNote }) {
         >
             <i class="fa-solid fa-play"></i>
         </button>
-        
+
     </fieldset>
 }
 
+function AddTodos({ info, emptyNoteToEdit, onInputChange }) {
+    const { title, todos } = info
+    const newTodos = todos ? todos : [{ txt: '', isDone: false }]
+    info.todos = newTodos
+
+    function onChangeTodos(info, name, newValue,idx) {
+        const newInfo = { ...info }
+
+        if (name === 'title') {
+            newInfo.title = newValue
+        } else {
+            newInfo.todos[idx][name] = newValue
+        }
+
+        onInputChange(emptyNoteToEdit, newInfo)
+    }
+
+    return <div className="note-content" >
+        <input
+            className="add-input title"
+            type='text'
+            name='title'
+            placeholder="Enter Title"
+            onChange={(ev) => onChangeTodos(info, 'title', ev.target.value)}
+            id="">
+
+        </input>
+
+        {newTodos.map((todo, idx) => {
+            return <div className="todo-list" key={todo.txt}>
+                <input
+                    className="add-input txt"
+                    type='text'
+                    placeholder="Enter Text"
+                    value={todo.txt}
+                    autoFocus={true}
+                    name='txt'
+                    onChange={ev => onChangeTodos(info, 'txt', ev.target.value,idx)}
+                    id="" />
+
+
+                <input type="checkbox"
+                    id={todo.txt}
+                    name='isDone'
+                    checked={todo.isDone}
+                    onChange={(ev) => onChangeTodos(info, 'isDone', ev.target.value,idx)} />
+
+            </div>
+        })}
+    </div>
+
+
+}
 function AddTxt({ info, emptyNoteToEdit, onInputChange }) {
 
     return <div className="add-txt">
@@ -81,7 +145,7 @@ function AddTxt({ info, emptyNoteToEdit, onInputChange }) {
 function DynamicAddNote(props) {
     const cmpMap = {
         'NoteTxt': <AddTxt {...props} />,
-        // 'NoteTodos': <NoteTodos {...props} />,
+        'NoteTodos': <AddTodos {...props} />,
         // 'NoteImg': <NoteImg {...props} />,
         // 'NoteVideo': <NoteVideo {...props} />,
         // 'NoteAudio': <NoteAudio {...props} />
