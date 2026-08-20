42e96528ab85869cce5aea12d6efa7668c7a8433
asanir96 <asanir96@gmail.com>
2026-08-20 09:47:58 +0300

    Refactor NoteFilter component to utilize useParams for type handling; update button rendering for active states

--- apps/note/cmps/NoteFilter.jsx
+++ apps/note/cmps/NoteFilter.jsx
@@ -1,5 +1,5 @@
 const { Link, NavLink } = ReactRouterDOM
-const { useSearchParams, useNavigate } = ReactRouterDOM
+const { useParams, useSearchParams, useNavigate } = ReactRouterDOM
 
 const { useState, useEffect } = React
 
@@ -7,23 +7,29 @@ const { useState, useEffect } = React
 export function NoteFilter({ filterBy, onSetFilterBy }) {
     const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
 
+    const params = useParams()
+    const type = params.type
+
     useEffect(() => {
         onSetFilterBy(filterByToEdit)
     }, [filterByToEdit])
 
-    function handleChange(ev) {
-        const { value, name, type } = ev.target
-        setFilterByToEdit(prev => ({ ...prev, [name]: value }))
+    function handleChange(value) {
+        setFilterByToEdit(prev => ({ ...prev, type: value }))
     }
 
 
     return <aside className="note-sidebar">
         <nav className="note-tabs">
-            <button className="note-tab-btn" name='type' value='' onClick={ev => handleChange(ev)}>Notes</button>
-            <button className="note-tab-btn" name='type' value='NoteTxt' onClick={ev => handleChange(ev)}>Text Notes</button>
-            <button className="note-tab-btn" name='type' value='NoteTodos' onClick={ev => handleChange(ev)}>Todo Notes</button>
-            <button className="note-tab-btn" name='type' value='NoteVideo' onClick={ev => handleChange(ev)}>Video Notes</button>
-            <button className="note-tab-btn" name='type' value='NoteAudio' onClick={ev => handleChange(ev)}>Audio Notes</button>
-        </nav> 
+            <button className={`note-tab-btn ${!filterByToEdit.type ? 'active' : ''}`} name='type' value='' onClick={ev => handleChange('')}>
+                <i class="fa-regular fa-lightbulb"></i></button>
+            <button className={`note-tab-btn ${filterByToEdit.type === 'NoteTxt' ? 'active' : ''}`} name='type' value='NoteTxt' onClick={ev => handleChange('NoteTxt')}>
+                <i class="fa-solid fa-pencil"></i></button>
+            <button className={`note-tab-btn ${filterByToEdit.type === 'NoteTodos' ? 'active' : ''}`} name='type' value='NoteTodos' onClick={ev => handleChange('NoteTodos')}>
+                <i class="fa-regular fa-square-check"></i></button>
+            <button className={`note-tab-btn ${filterByToEdit.type === 'NoteVideo' ? 'active' : ''}`} name='type' value='NoteVideo' onClick={ev => handleChange('NoteVideo')}>
+                <i class="fa-brands fa-youtube"></i></button>
+            <button className={`note-tab-btn ${filterByToEdit.type === 'NoteAudio' ? 'active' : ''}`} name='type' value='NoteAudio' onClick={ev => handleChange('NoteAudio')}><i class="fa-solid fa-volume"></i></button>
+        </nav>
     </aside>
 }
